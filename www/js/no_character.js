9af0b559c5649d9004c8d13620269b94c7f2af02
CodeSampleH6AN <harissestan91@gmail.com>
2018-11-28 18:15:20 +0100

    char

--- www/js/no_character.js
+++ www/js/no_character.js
@@ -1,23 +1,23 @@
-$('html').bind('keypress', function (e) {
-    if (e.keyCode === 32) {
+$('html').bind('keydown', function (e) {
+    if (e.keyCode == 32) {
         return false;
     }
-    if (e.keyCode === 45) {
+    if (e.keyCode == 45) {
         return false;
     }
-    if (e.keyCode === 109) {
+    if (e.keyCode == 109) {
         return false;
     }
-    if (e.keyCode === 173) {
+    if (e.keyCode == 173) {
         return false;
     }
-    if (e.keyCode === 188) {
+    if (e.keyCode == 188) {
         return false;
     }
-    if (e.keyCode === 189) {
+    if (e.keyCode == 189) {
         return false;
     }
-    if (e.keyCode === 190) {
+    if (e.keyCode == 190) {
         return false;
     }
 });
