af4513bfacb85329b6d00f0e81ca4f81a517c096
CodeSampleH6AN <harissestan91@gmail.com>
2018-11-28 17:19:07 +0100

    minus

--- www/js/no_character.js
+++ www/js/no_character.js
@@ -1,20 +1,20 @@
 $('html').bind('keypress', function (e) {
-    if (e.keyCode == 32) {
+    if (e.keyCode === 32) {
         return false;
     }
-    if (e.keyCode == 109) {
+    if (e.keyCode === 45) {
         return false;
     }
-    if (e.keyCode == 188) {
+    if (e.keyCode === 109) {
         return false;
     }
-    if (e.keyCode == 189) {
+    if (e.keyCode === 188) {
         return false;
     }
-    if (e.keyCode == 190) {
+    if (e.keyCode === 189) {
         return false;
     }
-    if (e.KEYCODE_MINUS = 69) {
+    if (e.keyCode === 190) {
         return false;
     }
 });
