6791b63dfe5cd7290ff0be4d76ceb356f71e3c2c
CodeSampleH6AN <harissestan91@gmail.com>
2018-11-28 17:33:03 +0100

    173

--- www/js/no_character.js
+++ www/js/no_character.js
@@ -8,6 +8,9 @@ $('html').bind('keypress', function (e) {
     if (e.keyCode === 109) {
         return false;
     }
+    if (e.keyCode === 173) {
+        return false;
+    }
     if (e.keyCode === 188) {
         return false;
     }
